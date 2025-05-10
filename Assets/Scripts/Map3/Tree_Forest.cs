f529a47ba897916fc94bc686d23c616fcd1d61ca
bamsiha <gmleh3991@gmail.com>
2025-05-10 19:32:16 +0900

    map3 seed gen logic

--- Assets/Scripts/Map3/Tree_Forest.cs
+++ Assets/Scripts/Map3/Tree_Forest.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,10 +6,18 @@ public class Tree_Forest : MonoBehaviour
 {
     private float HP = 100;
     public UnityEvent onLogged;
+
+    public GameObject _seed;
+    private float minSeedRegenTerm;
+    private float maxSeedRegenTerm;
+    private float curSeedRegenTerm;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        minSeedRegenTerm = 10;
+        maxSeedRegenTerm = 30;
+        curSeedRegenTerm = UnityEngine.Random.Range(minSeedRegenTerm, maxSeedRegenTerm);
     }
     private void OnEnable()
     {
@@ -20,6 +29,22 @@ public class Tree_Forest : MonoBehaviour
         if(HP <= 0)
         {
             onLogged.Invoke();
+        }
+
+        SeedRegen();
+    }
+
+    private void SeedRegen()
+    {
+        if (curSeedRegenTerm <= 0)
+        {
+            Vector3 randomVec = new Vector3(UnityEngine.Random.Range(2, 8), -this.transform.position.y + 1, UnityEngine.Random.Range(2, 8));
+            Instantiate(_seed, this.transform.position + randomVec, Quaternion.identity);
+            curSeedRegenTerm = UnityEngine.Random.Range(minSeedRegenTerm, maxSeedRegenTerm);
+        }
+        else
+        {
+            curSeedRegenTerm -= Time.deltaTime;
         }
     }
 
